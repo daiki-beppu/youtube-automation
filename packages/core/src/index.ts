0e8b0a652aa92125088bf9ed34655018daee82f5
daiki-beppu <beppu.engineer@gmail.com>
2026-06-03 09:03:18 +0900

    takt: issue-734-core-utils-exception

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,3 +1,15 @@
 // Skeleton entry for the core package. Real domain logic is migrated in later #727 issues.
 export const greeting = (): string =>
   "youtube-channels-automation core (TS rewrite skeleton)";
+
+export {
+  AuthError,
+  AutomationError,
+  ConfigError,
+  GeneratorError,
+  QuotaExhaustedError,
+  UploadError,
+  ValidationError,
+  YouTubeAPIError,
+  type YouTubeAPIErrorOptions,
+} from "./errors.ts";
